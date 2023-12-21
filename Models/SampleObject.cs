baf34ad3ce4294181c68526f9e9da538f4476095
MinPhyoeMinThu <121812138+DejavuNerdPro@users.noreply.github.com>
2023-12-21 22:39:26 +0630

    All component cases

---
+++ Models/SampleObject.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class SampleObject
+    {
+        public string DisplayName { get; set; }
+        public int Age { get; set; }
+    }
+}
